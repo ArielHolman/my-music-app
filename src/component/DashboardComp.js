66474be6cd67aa52d95bb1d78d1a1ab228baccbe
ArielHolman <ArielARodrigues01@gmail.com>
2020-09-28 19:27:51 -0500

    added files for components, created login comp and nav bar.

---
+++ src/component/DashboardComp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DashboardComp() {
+  return (
+    <div>
+      <p>I Works!</p>
+    </div>
+  )
+}
+
+export default DashboardComp
---
+++ src/component/NavBar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  withoutLabel: {
+    marginTop: theme.spacing(3),
+  },
+  textField: {
+    width: '20ch',
+  },
+}));
+
+export default function NavBar() {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              My Music App
+            </Typography>
+          </Toolbar>
+        </AppBar>
+    </div>
+  )
+}
